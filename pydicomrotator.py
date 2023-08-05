616dd6e4d308ab0f149eb1e32c2d809a0424accd
mr-xak <salonf@mrrc.obninsk.ru>
2023-08-05 18:31:41 +0300

    all logic, need to create new DICOM instead of using old template

--- pydicomrotator.py
+++ pydicomrotator.py
@@ -7,13 +7,26 @@ from pydicom.uid import UID
 from time import strftime, localtime
 from PIL import Image
 from pydicom._storage_sopclass_uids import SecondaryCaptureImageStorage
+from pydicom._uid_dict import UID_dictionary
+import uuid
 
 baseUID = '1.2.826.0.1.3680043.10.594'
 ImplementationClassUID = '2.25.229451600072090404564544894284998027172'
 
+def generateUID(_uuid=None):
+    """Returns a new DICOM UID based on a UUID, as specified in CP1156 (Final)."""
+    if _uuid is None:
+        _uuid = uuid.uuid1()
+    return "2.25.%i" % _uuid.int
+
+def getPDCMUID(name):
+    # print("{" + "\n".join("{}: {}".format(k, v) for k, v in dicom.UID.UID_dictionary.items()) + "}")
+    return [k for k, v in UID_dictionary.items() if v[0] == name][0]
+
 def getEmptyDataset(filename : str, uid : pydicom.uid.UID):
     file_meta = pydicom.dataset.Dataset()
-    file_meta.MediaStorageSOPClassUID = UID('1.2.840.10008.5.1.4.1.1.2')
+    #file_meta.MediaStorageSOPClassUID = UID('1.2.840.10008.5.1.4.1.1.2')
+    file_meta.MediaStorageSOPClassUID = getPDCMUID("CT Image Storage")
     file_meta.MediaStorageSOPInstanceUID = uid
     file_meta.ImplementationClassUID = ImplementationClassUID
     file_meta.TransferSyntaxUID = pydicom.uid.ImplicitVRLittleEndian
@@ -41,22 +54,50 @@ def createNewDataset(npData : np.ndarray, dataMeta : dict, target : str, templat
     spacing = [dataMeta['zSize'], dataMeta['ySize']]
     uid = genNewUID()
     print(npNewData.shape)
-    template.Rows = Rows
-    template.Columns = Cols
-    template.PixelSpacing = spacing
-    template.SliceThickness = dataMeta['xSize']
-    template.SOPClassUID = SecondaryCaptureImageStorage
     #template.FrameOfReferenceUID = UID("%s.0" % (uid, ))
     #template.ReferencedImageSequence[0].ReferencedSOPInstanceUID = UID("%s.0" % (uid, ))
+    ptBaseUID = generateUID()
     for i in range(imgMax):
         newName = "IM%03d.dcm" % (i, )
+        sopInstanceUID = "%s.%i" % (ptBaseUID, i)
+        template2 = getEmptyDataset(newName, sopInstanceUID)
+        template2.SeriesInstanceUID = template.SeriesInstanceUID
+        template2.StudyInstanceUID = template.StudyInstanceUID
+        template2.Modality = "CT"
+        template2.SOPClassUID = getPDCMUID("CT Image Storage")
+        template2.Rows = Rows
+        template2.Columns = Cols
+        template2.PixelSpacing = spacing
+        template2.SliceThickness = dataMeta['xSize']
+        #template2.SOPClassUID = SecondaryCaptureImageStorage
         #template.file_meta = getEmptyDataset(newName, UID("%s.%s" % (uid, str(i))))
-        template.SliceLocation = i * dataMeta['xSize']
-        template.ImagePositionPatient = [0, 0, i * dataMeta['xSize']]
-        template.SOPInstanceUID = UID("%s.%s" % (uid, str(i)))
+        #template2.FrameOfReferenceUID = UID("%s.0" % (uid, ))
+        #template2.FrameOfReferenceUID = "%s.%i" % (ptBaseUID, 0)
+    #template.ReferencedImageSequence[0].ReferencedSOPInstanceUID = UID("%s.0" % (uid, ))
+        template2.SliceLocation = i * dataMeta['xSize']
+        template2.ImagePositionPatient = [0.0, 0.0, i * dataMeta['xSize']]
+        #template2.PatientPosition = [0.0, 0.0, i * dataMeta['xSize']]
+        template2.PatientPosition = template.PatientPosition
+        template2.ImageOrientationPatient = [1, 0, 0, 0, 1, 0]
+        #template2.SOPInstanceUID = UID("%s.%s" % (uid, str(i)))
+        #template2.SOPInstanceUID = getPDCMUID("CT Image Storage")
+        template2.SOPInstanceUID = sopInstanceUID
+        template2.ImageType = "ORIGINAL\SECONDARY\AXIAL"
+        template2.SamplesPerPixel = 1
+        template2.PhotometricInterpretation = "MONOCHROME2"
+        template2.BitsAllocated = 16
+        template2.BitsStored = 16
+        template2.HighBit = 15
+        template2.KVP = ""
+        template2.AcquisitionNumber = ""
+        template2.PixelRepresentation = template.PixelRepresentation
+
+        template2.RescaleSlope = dataMeta['slope']
+        template2.RescaleIntercept = dataMeta['intercept']
         #template.pixel_array = npData[i, :, :].tobytes()
-        template.PixelData = Image.fromarray(npData[i, :, :]).tobytes()
-        template.save_as(os.path.join(target, newName), write_like_original=False)
+        template2.PixelData = npNewData[i, :, :].tobytes()
+        #template2.PixelData = Image.fromarray(npNewData[i, :, :]).tobytes()
+        template2.save_as(os.path.join(target, newName), write_like_original=False)
 
 
     pass
@@ -99,7 +140,7 @@ def runner(input : str,  target : str, dummy : bool):
                 flD.file_meta.TransferSyntaxUID = '1.2.840.10008.1.2'
             if not isMetaSet:
                 dataMeta = extractFeatures(flD)     
-                template = flD
+                template = flD.copy()
                 isMetaSet = True
             pos = float(flD.ImagePositionPatient[2])
             zs.append(pos)
@@ -111,6 +152,7 @@ def runner(input : str,  target : str, dummy : bool):
     npData = np.array(datas)
     print(npData.shape)
     print(type(template))
+    #del template.file_meta
     dataMeta.update({'zLen': npData.shape[0]})
 
 
